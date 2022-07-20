6b377b4a21918c41c243bbf818e53ae456f8d2c3
Patrick Martins <c.martins13@outlook.com>
2022-07-20 19:25:29 +0000

    Added the first tag

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,7 +9,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="https://noticiasdatv.uol.com.br/media/_versions/everybody-hates-chris-julius-dia-dos-pais_fixed_large.jpg"></img></center>
   <center><h2>Meow World!</h2></center>
   <center>Welcome to ${PREFIX}'s app. NÃO TO BOM NÃO OAAAAAAAAAAAAAAAAAA.</center>
   <!-- END -->
